ecd7b03546ba4aa1b277bedd938dbe0ab8c34cac
Евгения <Evgenia@MacBook-Pro-Evgenia.local>
2022-11-29 10:18:14 +0300

    Task about check corrected

--- HW1/logic check.py
+++ HW1/logic check.py
@@ -1,23 +1,6 @@
 # Напишите программу для проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 
-# A = [
-#         [False, False, False],
-#         [False, False, True],
-#         [False, True, False],
-#         [False, True, True],
-#         [True, False, False],
-#         [True, False, True],
-#         [True, True, False],
-#         [True, True, True]
-# ]
-#
-# elem = A[0]
-# while elem in len(A):
-#     print (not False in elem)
-#     elem+=1
-
-x = input("Введите x: ") # Не дано, какой тип данных будем проверять, поэтому конвертировать не будем
-y = input("Введите y: ")
-z = input("Введите z: ")
-
-print(not (x or y or z) == (not x and not y and not z))
+for x in [True, False]:
+    for y in [True, False]:
+        for z in [True, False]:
+            print(f"x = {x}, y = {y}, z = {z}, result = {not (x or y or z) == (not x and not y and not z)}")
